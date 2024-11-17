5e27d3dd8830a9cb0dac77a78fda40fd695ff8da
Joseph Pouradier Duteil <joseph.pouradier-duteil@cpe.fr>
2024-11-17 14:41:27 +0100

    perfect: the client only connect to the socket when needed

--- nodejs-app/www/index.js
+++ nodejs-app/www/index.js
@@ -1,20 +1,31 @@
-let socket = io();
 let counterDiv = 0;
-socket.on("notify", function (msg) {
-    console.log(msg);
-    console.log(typeof msg);
-    let elem = document.createElement("div");
-    elem.setAttribute("class", "personne");
-    elem.setAttribute("id", counterDiv);
-    counterDiv++;
-    elem.innerHTML = msg;
-    document.getElementById("personnes").appendChild(elem);
-});
 
 function go() {
     let name = document.getElementById("nom").value;
     let prenom = document.getElementById("prenom").value;
     console.log("POST /", name, prenom);
+
+    // Connect to the socket
+    let socket = io();
+
+    // Set up the listener for the "notify" event
+    socket.on("notify", function (msg) {
+        console.log(msg);
+        console.log(typeof msg);
+
+        let elem = document.createElement("div");
+        elem.setAttribute("class", "personne");
+        elem.setAttribute("id", counterDiv);
+        counterDiv++;
+        elem.innerHTML = msg;
+        document.getElementById("personnes").appendChild(elem);
+
+        // Disconnect the socket after receiving the message
+        socket.disconnect();
+        console.log("Socket disconnected after receiving message.");
+    });
+
+    // Send the POST request
     fetch("/", {
         method: "POST",
         headers: {
@@ -27,3 +38,4 @@ function go() {
             console.log("POST: result=", res);
         });
 }
+
